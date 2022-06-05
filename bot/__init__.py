7d1561628524ddba2dc110a23b4d5650cde85776
Christopher Hart <christopherjhart95@gmail.com>
2022-06-04 20:28:40 -0400

    Add version to /hello command

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,3 @@
 """Contains all code related to the Discord bot itself."""
+
+__version__ = "0.2.0"
--- bot/embeds/hello.py
+++ bot/embeds/hello.py
@@ -1,6 +1,7 @@
 """/hello user feedback embeds."""
 
 from disnake import Embed
+from bot import __version__
 from .general import (
     command_ok,
 )
@@ -16,7 +17,7 @@ def hello_ok_reply(embed: Embed, latency: float) -> Embed:
         embed (Embed): Embed to modify.
         latency (float): Discord API latency measured in seconds.
     """
-    embed.add_field(name="Version", value="v0.0.0", inline=False)
+    embed.add_field(name="Version", value=f"v{__version__}", inline=False)
     embed.add_field(
         name="Discord API Latency", value=f"{(latency * 1000):.2f} ms", inline=False
     )
